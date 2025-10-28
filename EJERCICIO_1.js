8054f9502e89c18d6bab0994625acdf6eeb21346
karol <karoln.oficiall@gmail.com>
2025-10-27 22:39:10 -0500

    Ejercicio1_D

---
+++ EJERCICIO_1.js
@@ -0,0 +1,9 @@
+for (let numero = 1; numero <= 20; numero++) {
+  if (numero % 3 === 0) {
+    console.log(numero);
+  }
+}
+
+//for crea el ciclo desde 1 hasta 20.//
+//El if (numero % 3 === 0) revisa si el número es múltiplo de 3.//
+
