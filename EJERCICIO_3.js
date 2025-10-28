5b087d4245a8871d26588bb8a60dfd1985f7e7c4
karol <karoln.oficiall@gmail.com>
2025-10-27 22:58:55 -0500

    Ejercicio3_D

---
+++ EJERCICIO_3.js
@@ -0,0 +1,13 @@
+for (let i = 1; i <= 10; i++) {
+  let numero = Number(prompt("Ingresa el número " + i + ":"));
+
+  if (numero > 50) {
+    console.log("El número " + numero + " es mayor que 50.");
+  }
+}
+
+//prompt pide cada número (convertido a número con Number)//
+
+//Si el valor ingresado es mayor a 50, se muestra en consola//
+
+//Si no, no se muestra nada//
